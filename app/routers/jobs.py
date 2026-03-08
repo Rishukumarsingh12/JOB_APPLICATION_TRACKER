ca129b0a9fd86e2808025c28294a1e6bb1f16553
Rishukumarsingh12 <rishusingh8671@gmail.com>
2026-03-09 02:38:20 +0530

    Add Job Stats Feature

--- app/routers/jobs.py
+++ app/routers/jobs.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends, HTTPException, Query
+from typing import Optional
+from sqlalchemy.orm import  Session
 from .. import models, schemas
 from ..dependencies import get_db, get_current_user
-
+from sqlalchemy import func
 
 router = APIRouter(prefix="/jobs", tags=["Jobs"])
 
@@ -41,11 +42,21 @@ def create_job(job: schemas.JobCreate,
 # Get all jobs of current user
 @router.get("/")
 def get_jobs(db: Session = Depends(get_db),
+             status: Optional[str] = Query(None),
+             company: Optional[str] = Query(None),
              current_user: models.User = Depends(get_current_user)):
 
     jobs = db.query(models.Job).filter(
         models.Job.user_id == current_user.id
-    ).all()
+    )
+
+    if status:
+        jobs = jobs.filter(models.Job.status == status)
+
+    if company:
+        jobs = jobs.filter(models.Job.company.ilike(f"%{company}%"))
+
+    jobs = jobs.all()
 
     return {
     "success": True,
@@ -105,3 +116,24 @@ def delete_job(job_id: int,
     "success": True,
     "message": "Job deleted successfully"
 }
+
+@router.get("/stats")
+def job_stats(
+    db: Session = Depends(get_db),
+    current_user: models.User = Depends(get_current_user)
+):
+
+    stats = (
+        db.query(models.Job.status, func.count(models.Job.id))
+        .filter(models.Job.user_id == current_user.id)
+        .group_by(models.Job.status)
+        .all()
+    )
+
+    result = {status: count for status, count in stats}
+
+    return {
+        "success": True,
+        "message": "Job statistics fetched successfully",
+        "data": result
+    }
